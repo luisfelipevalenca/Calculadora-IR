b65c421461c26d3ffd01c2292f9e934cdcab6bba
Luís Felipe Valença de Albuquerque <37195599+luisfelipevalenca@users.noreply.github.com>
2025-05-13 14:17:42 -0300

    Update calcula_IR.py

--- calcula_IR_simples.py
+++ calcula_IR_simples.py
@@ -1,37 +1,36 @@
-while True:
-    operation = input('Digite a opção desejada: \n 1. Calcular IR (Imposto de Renda) \n')
-    if operation == '1':
-        break
+print("""
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$                                               $ 
+$                                               $  
+$         Calculadora de desconto de IR         $
+$                                               $ 
+$                                               $ 
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+""")
 
-print('-' * 50)
+controle = True
+while controle:
+    leitura = input("Informe o salário: R$").replace(",",".")
+    try:
+        salario = float(leitura)
+        controle = False
 
-salario_input = input('Digite seu salário bruto: R$ ').replace(",", ".")
-print('-' * 50)
+    except ValueError:
+        print("Entrada inválida! Tente novamente.\n")
 
-try:
-    salario = float(salario_input)
+desconto = 0
 
-    if salario <= 2428.80:
-        aliquota = 0
-    elif salario <= 2826.65:
-        aliquota = 7.5
-    elif salario <= 3751.05:
-        aliquota = 15.0
-    elif salario <= 4664.68:
-        aliquota = 22.5
-    else:
-        aliquota = 27.5
+if salario <= 2428.80:
+      print("Não possui desconto de IR")
+elif salario <= 2826.65:
+      desconto = (salario * (7.5/100))
+elif salario <= 3751.05:
+      desconto = (salario * (15/100))
+elif salario <= 4664.68:
+     desconto = (salario * (22.5/100))
+else:
+     desconto = (salario * (27.5/100))
 
-    desconto = salario * (aliquota / 100)
-    liquido = salario - desconto
-
-    print('Resultado do cálculo de IR:')
-    print(f"Salário Bruto: R$ {salario:.2f}")
-    print(f"Alíquota Aplicada: {aliquota}%")
-    print(f"Desconto IR: R$ {desconto:.2f}")
-    print(f"Salário Líquido: R$ {liquido:.2f}")
-
-except ValueError:
-    print('Entrada inválida. Digite um número válido para o salário.')
-
-print('-' * 50)
+salario = salario - desconto
+print(f"Valor do desconto de IR: R${desconto:.2f}")
+print(f"Salário líquido: R${salario:.2f}")
